5df5dd483da869a4290dc0f119bf7a3b0a10b8f2
Test <thomsmanly@gmail.com>
2026-08-03 20:00:16 +0000

    Mise a jour url

--- src/app/interfaces/environment.ts
+++ src/app/interfaces/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
 
     production: false,
 
-    apiUrl: "https://ce64-102-180-234-24.ngrok-free.app/api"
+    apiUrl: "https://5ed3-102-180-60-64.ngrok-free.app/api"
 
 
     // apiUrl: "http://localhost:8888/api"
